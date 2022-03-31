fb123daf1c2473f0a841a77d8d2b5a0f5ae497c6
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-04-01 04:00:15 +0530

    Initial Commit.

---
+++ StarZone/Classes/ViewControllers/Landing/LandingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LandingViewController.swift
+//  StarZone
+//
+//  Created by Ravindu Wataketiya on 2022-04-01.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    @IBOutlet weak var btnSignup: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnSignupClick(_ sender: Any) {
+        self.performSegue(withIdentifier: "gotoSignup", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
